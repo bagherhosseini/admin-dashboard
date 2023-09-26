118b1a063b31171ab4c16fdc83636c0d4dc40504
Bagher hosseini <bagherhosseini03@gmail.com>
2023-09-26 14:44:45 +0200

    feat(auth): sign in setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button"
-
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
