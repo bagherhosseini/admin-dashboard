a750b7abc94823e766d5b7f77bd21749d2097d08
Jirawan <jirawan.sangthong@yh.nackademin.se>
2023-09-29 15:30:29 +0200

    feat(): fetching postdatadb endpoint

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
+"use client";
 import { UserButton } from "@clerk/nextjs";
- 
+import React, { useState } from "react";
+
 export default function Home() {
+  const [state, setState] = useState<string>("");
+
+  const apiReq = async () => {
+    const res = await fetch("/api/PostDataDB", {
+      method: "POST",
+      body: JSON.stringify({
+        title: "TEST2",
+        price: 100,
+        description: "test2",
+        img: "scam",
+      }),
+    });
+    const json = await res.json();
+    // setState(json);
+    console.log(json);
+  };
+
   return (
     <div>
-      <UserButton afterSignOutUrl="/"/>
+      <UserButton afterSignOutUrl="/" />
+      <button onClick={apiReq}>click</button>
+      <input type="text" name="" id="" />
+      {/* <p>{state}</p> */}
     </div>
-  )
+  );
 }
