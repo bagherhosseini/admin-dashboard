4fca7cefa66a168f4eb8080877e3eb0f499c8c5d
Bagher hosseini <bagherhosseini03@gmail.com>
2023-10-02 14:52:36 +0200

    refactor: new fetch data exemple

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,17 @@ export default function Home() {
   const [state, setState] = useState<string>("");
 
   const apiReq = async () => {
-    const res = await fetch("/api/PostDataDB", {
+    const res = await fetch("/api/addProductEndpoint", {
       method: "POST",
       body: JSON.stringify({
         title: "TEST2",
         price: 100,
         description: "test2",
         img: "scam",
+        categoryId: 1,
+        archived: false,
+        featured: false,
+        sizeId: 1,
       }),
     });
     const json = await res.json();
