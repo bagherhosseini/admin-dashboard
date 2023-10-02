83a91077d5f16ad6197e93f0dbee4accb5fa4004
Bagher hosseini <bagherhosseini03@gmail.com>
2023-10-02 13:51:09 +0200

    feat(add product): validation for adding product

--- app/api/addProductEndpoint/route.ts
+++ app/api/addProductEndpoint/route.ts
@@ -1,9 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
 import prismadb from "../../../lib/prismadb";
+import { z } from "zod";
 
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
-    const { title, price, description, img } = await req.json();
+    const { title, price, description, img, categoryId, archived, featured, sizeId } = await req.json();
+
+    // Define a schema to validate the request body against
+    const ProductValidation = z.object({
+      title: z.string().min(3),
+      price: z.number(),
+      description: z.string(),
+      img: z.string(),
+      categoryId: z.number(),
+      archived: z.boolean(),
+      featured: z.boolean(),
+      sizeId: z.number(),
+    });
+
+    // Validate the request body against the schema above
+    ProductValidation.parse({ title, price, description, img, categoryId, archived, featured, sizeId });
+
+    // extract the inferred type
+    type ProductValidation = z.infer<typeof ProductValidation>;
+
     // Create a new product in the database
     const newProduct = await prismadb.product.create({
       data: {
@@ -11,13 +31,28 @@ export async function POST(req: NextRequest, res: NextResponse) {
         price,
         description,
         img,
+        categoryId,
+        archived,
+        featured,
+        sizeId,
       },
     });
-    return new Response(JSON.stringify(newProduct), { status: 200 });
+
+    return new Response(JSON.stringify({newProduct, message: "Success"}), { status: 200 });
   } catch (error) {
     console.error(error);
-    return new Response(JSON.stringify({ error: "An error occurred" }), {
-      status: 500,
-    });
+
+    if (error instanceof z.ZodError) {
+      // If it's a ZodError (validation error), extract the messages and return them in the response
+      const validationMessages = error.errors.map((validationError) => validationError.message);
+      return new Response(JSON.stringify({ errors: validationMessages[0] }), {
+        status: 400,
+      });
+    } else {
+      // Handle other errors here if needed
+      return new Response(JSON.stringify({ error: "An error occurred" }), {
+        status: 500,
+      });
+    }
   }
 }
