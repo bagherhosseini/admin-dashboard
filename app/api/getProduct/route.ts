f9abe5641c8f02cf304e3fadc36d09b9967233ed
Jirawan <jirawan.sangthong@yh.nackademin.se>
2023-10-03 15:51:45 +0200

    feat(): created endpoint to get products

---
+++ app/api/getProduct/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import prismadb from "../../../lib/prismadb";
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  try {
+    const products = await prismadb.product.findMany({});
+    return new Response(JSON.stringify({ products }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response(
+      JSON.stringify({ error: "An error occurred", errorMessage: error }),
+      {
+        status: 500,
+      }
+    );
+  }
+}
