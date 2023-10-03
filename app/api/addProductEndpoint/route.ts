434655377d14f74f120a9375bae18bd63cb857e0
Jirawan <jirawan.sangthong@yh.nackademin.se>
2023-10-03 13:47:16 +0200

    fix(type): fixing type error

--- app/api/addProductEndpoint/route.ts
+++ app/api/addProductEndpoint/route.ts
@@ -1,53 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import prismadb from "../../../lib/prismadb";
 import { z } from "zod";
+import { title } from "process";
 
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
     const data = await req.json();
 
-  if (
-    typeof data.title === 'string' &&
-    typeof data.price === 'number' &&
-    typeof data.description === 'string' &&
-    typeof data.img === 'string' &&
-    typeof data.categoryId === 'number' &&
-    typeof data.archived === 'boolean' &&
-    typeof data.featured === 'boolean' &&
-    typeof data.sizeId === 'number'
-  ) {
-    const {
-      title,
-      price,
-      description,
-      img,
-      categoryId,
-      archived,
-      featured,
-      sizeId,
-  } = data;
-
-    // Define a schema to validate the request body against
-    const ProductValidation = z.object({
-      title: z.string().min(3),
-      price: z.number(),
-      description: z.string(),
-      img: z.string(),
-      categoryId: z.number(),
-      archived: z.boolean(),
-      featured: z.boolean(),
-      sizeId: z.number(),
-    });
-
-    // Validate the request body against the schema above
-    ProductValidation.parse({ title, price, description, img, categoryId, archived, featured, sizeId });
-
-    // extract the inferred type
-    type ProductValidation = z.infer<typeof ProductValidation>;
-
-    // Create a new product in the database
-    const newProduct = await prismadb.product.create({
-      data: {
+    if (
+      typeof data.title === "string" &&
+      typeof data.price === "number" &&
+      typeof data.description === "string" &&
+      typeof data.img === "string" &&
+      typeof data.categoryId === "number" &&
+      typeof data.archived === "boolean" &&
+      typeof data.featured === "boolean" &&
+      typeof data.sizeId === "number"
+    ) {
+      const {
         title,
         price,
         description,
@@ -56,16 +26,70 @@ export async function POST(req: NextRequest, res: NextResponse) {
         archived,
         featured,
         sizeId,
-      },
-    });
+      } = data as {
+        title: string;
+        price: number;
+        description: string;
+        img: string;
+        categoryId: number;
+        archived: boolean;
+        featured: boolean;
+        sizeId: number;
+      };
 
-    return new Response(JSON.stringify({newProduct, message: "Success"}), { status: 200 });
+      // Define a schema to validate the request body against
+      const ProductValidation = z.object({
+        title: z.string().min(3),
+        price: z.number(),
+        description: z.string(),
+        img: z.string(),
+        categoryId: z.number(),
+        archived: z.boolean(),
+        featured: z.boolean(),
+        sizeId: z.number(),
+      });
+
+      // Validate the request body against the schema above
+      ProductValidation.parse({
+        title,
+        price,
+        description,
+        img,
+        categoryId,
+        archived,
+        featured,
+        sizeId,
+      });
+
+      // extract the inferred type
+      type ProductValidation = z.infer<typeof ProductValidation>;
+
+      // Create a new product in the database
+      const newProduct = await prismadb.product.create({
+        data: {
+          title,
+          price,
+          description,
+          img,
+          categoryId,
+          archived,
+          featured,
+          sizeId,
+        },
+      });
+
+      return new Response(JSON.stringify({ newProduct, message: "Success" }), {
+        status: 200,
+      });
+    }
   } catch (error) {
     console.error(error);
 
     if (error instanceof z.ZodError) {
       // If it's a ZodError (validation error), extract the messages and return them in the response
-      const validationMessages = error.errors.map((validationError) => validationError.message);
+      const validationMessages = error.errors.map(
+        (validationError) => validationError.message
+      );
       return new Response(JSON.stringify({ errors: validationMessages[0] }), {
         status: 400,
       });
