a34371a56caed7d7a000192ed15da92d6c513813
Gustav Franchell <gfranchell@gmail.com>
2023-10-04 18:12:54 +0200

    style(billboards): some style for billboards

---
+++ app/(pages)/billboards/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <div className="flex">
+      <h1>Billboards</h1>
+      <Button>Button</Button>
+    </div>
+  );
+}
