ed94023408fbdb7f5daedaf39aa3dd25af03e6f7
Gustav Franchell <gfranchell@gmail.com>
2023-10-06 15:43:49 +0200

    feat(billboards): create Link to create new

--- app/(pages)/billboards/page.tsx
+++ app/(pages)/billboards/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 import React from "react";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <div className="flex">
       <h1>Billboards</h1>
-      <Button>Button</Button>
+      <Link href="/billboards/new" legacyBehavior passHref>
+      <Button>
+        Create new billboard</Button>
+        </Link>
     </div>
   );
 }
