c667bb6448b0985d0b7e0adc71039c146f57ec75
Bagher hosseini <bagherhosseini03@gmail.com>
2023-10-08 18:46:55 +0200

    chore(prisma): Update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = { 
+    extends: ['@commitlint/config-conventional'] ,
+    rules: {
+        'subject-case': [0, 'always', 'upper-case'],
+        'scope-case': [0, 'always', 'upper-case'],
+    },
+};
