1666ae9ed705e6add9afe0548c9f9f3fe16a07f1
Gustav Franchell <gfranchell@gmail.com>
2023-10-10 16:16:56 +0200

    feat(sizes): DELETE sizes from database

---
+++ app/api/(size)/deleteSize/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import prismadb from "../../../../lib/prismadb";
+
+export async function DELETE(req: NextRequest, res: NextResponse) {
+  try {
+    const { id } = await req.json();
+    // Perform the deletion using prismadb
+    const deletedSize = await prismadb.size.delete({ where: 
+        { id: id  } });
+    
+    // Check if any rows were deleted
+    if (deletedSize) {
+      return new Response(JSON.stringify({ message: "Size deleted successfully" }), {
+        status: 200,
+      });
+    } else {
+      return new Response(JSON.stringify({ message: "No size found to delete" }), {
+        status: 404,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return new Response(
+      JSON.stringify({ error: "An error occurred", errorMessage: error }),
+      {
+        status: 500,
+      }
+    );
+  }
+}
