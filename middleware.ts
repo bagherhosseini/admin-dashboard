3b7e9017bc3bdb846d50cd557dd99cb2eec36845
Gustav Franchell <gfranchell@gmail.com>
2023-10-10 16:12:48 +0200

    feat(sizes): add public routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // "/" will be accessible to all users
-  publicRoutes: ["/api/addProduct", "/api/getProduct", "/api/order", "/api/getProductById"],
+  publicRoutes: ["/api/addProduct", "/api/getProduct", "/api/order", "/api/getProductById", "/api/addSize", "/api/getSize", "/api/getSizeById"],
 });
 
 export const config = {
