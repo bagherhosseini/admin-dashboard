c66b1c9fea51ef1465aca54cc49157827e8a7d44
Bagher hosseini <bagherhosseini03@gmail.com>
2023-10-10 19:20:06 +0200

    chore: modify store switcher

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -22,15 +22,37 @@ import {
     PopoverTrigger,
 } from "@/components/ui/popover"
 
+import { useParams, useRouter } from "next/navigation"
 import * as React from "react"
+
+import {
+    BackpackIcon,
+    CheckIcon,
+    CaretSortIcon,
+    PlusCircledIcon
+} from "@radix-ui/react-icons"
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
 
 interface StoreSwitcherProps extends PopoverTriggerProps {
-  items: Record<string, any>[];
+    items: Record<string, any>[];
 }
 
 export default function StoreSwitcher({ className, items = [] }: StoreSwitcherProps) {
+    const params = useParams();
+    const router = useRouter();
+
+    const formattedItems = items.map((item) => ({
+        label: item.name,
+        value: item.id
+    }));
+
+    const currentStore = formattedItems.find((item) => item.value === params.storeId);
+
+    const onStoreSelect = (store: { value: string, label: string }) => {
+
+        router.push(`/${store.value}`);
+    };
 
     return (
         <Popover>
@@ -41,8 +63,10 @@ export default function StoreSwitcher({ className, items = [] }: StoreSwitcherPr
                     role="combobox"
                     aria-label="Select a store"
                     className={cn("w-[200px] justify-between", className)}
-                    >
-                        Store name
+                >
+                    <BackpackIcon className="mr-2 h-4 w-4" />
+                    {currentStore?.label}
+                    <CaretSortIcon className="ml-auto h-4 w-4 shrink-0 opacity-50" />
                 </Button>
             </PopoverTrigger>
             <PopoverContent className="w-[200px] p-0">
@@ -51,11 +75,36 @@ export default function StoreSwitcher({ className, items = [] }: StoreSwitcherPr
                     <CommandList>
                         <CommandEmpty>No results found.</CommandEmpty>
                         <CommandGroup heading="Stores">
-                            <CommandItem>Calendar</CommandItem>
+                            {formattedItems.map((store) => (
+                                <CommandItem
+                                    key={store.value}
+                                    onSelect={() => onStoreSelect(store)}
+                                    className="text-sm"
+                                >
+                                    <BackpackIcon className="mr-2 h-4 w-4" />
+                                    {store.label}
+                                    <CheckIcon
+                                        className={cn(
+                                            "ml-auto h-4 w-4",
+                                            currentStore?.value === store.value
+                                                ? "opacity-100"
+                                                : "opacity-0"
+                                        )}
+                                    />
+                                </CommandItem>
+                            ))}
                         </CommandGroup>
                         <CommandSeparator />
                         <CommandGroup>
-                            <CommandItem>Profile</CommandItem>
+                            <CommandItem
+                                // onSelect={() => {
+                                //     setOpen(false)
+                                //     storeModal.onOpen()
+                                // }}
+                            >
+                                <PlusCircledIcon className="mr-2 h-5 w-5" />
+                                Create Store
+                            </CommandItem>
                         </CommandGroup>
                     </CommandList>
                 </Command>
