6f0180b8e16d865b643356010cd970643abbbc78
KungLucas <lucas.larsson93@hotmail.com>
2023-10-11 23:04:51 +0200

    feat(): route to view all categories using the get API.

---
+++ app/(pages)/[storeId]/categories/page.tsx
@@ -0,0 +1,41 @@
+import { CategoryColumn } from "./components/columns"
+import { CategoryClient } from "./components/client";
+import axios from 'axios';
+import { format } from "date-fns";
+
+const CategoryPage = async ({
+  params
+}: {
+  params: { storeId: string }
+}) => {
+  const categoriesFetch = async () => {
+    try {
+      const response = await axios.post("http://localhost:3000/api/getCategory", {
+        storeId: params.storeId
+      });
+      return response.data
+    } catch (error) {
+      console.error("Error fetching sizes:", error);
+      throw error;
+    }
+  };
+
+  const categories = await categoriesFetch();
+console.log(categories.categories);
+  if(categories.categories.length !== 0) {
+    const formattedCategories: CategoryColumn[] = categories.categories.map((item: CategoryColumn) => ({
+      id: item.id,
+      name: item.name,
+      description: item.description,
+    }));
+
+    return (
+      <div className="flex-col">
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <CategoryClient data={formattedCategories} />
+        </div>
+      </div>
+    );
+  }
+};
+export default CategoryPage;
