65b9be5abd6c746ba458a8a4a112664cc8f3693f
Gustav Franchell <gfranchell@gmail.com>
2023-10-11 17:02:22 +0200

    fix(size): fixed backend creating Size

--- app/api/(size)/addSize/route.ts
+++ app/api/(size)/addSize/route.ts
@@ -4,35 +4,24 @@ import { z } from "zod";
 
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
+    const data = await req.json();
     const {
+      storeId,
       name,
       description,
-      storeId,
-    }  = await req.json();
-    // Define a schema to validate the request body against
-    const SizeValidation = z.object({
-        name: z.string().min(1),
-        description: z.string().min(3),
-        storeId: z.string().min(3),
-      
-    });
+    } = data as {
+      storeId: string;
+      name: string;
+      description: string;
+    };
 
-    // Validate the request body against the schema above
-    SizeValidation.parse({
-      name,
-      description,
-      storeId,
-    });
 
-    // extract the inferred type
-    type SizeValidation = z.infer<typeof SizeValidation>;
-
-    // Create a new sizes in the database
+    // Create a new size in the database
     const newSize = await prismadb.size.create({
       data: {
+        storeId,
         name,
         description,
-        storeId,
       },
     });
 
@@ -40,21 +29,10 @@ export async function POST(req: NextRequest, res: NextResponse) {
       status: 200,
     });
   } catch (error) {
-    console.error(error);
+    console.error("server: ", error);
 
-    if (error instanceof z.ZodError) {
-      // If it's a ZodError (validation error), extract the messages and return them in the response
-      const validationMessages = error.errors.map(
-        (validationError) => validationError.message
-      );
-      return new Response(JSON.stringify({ errors: validationMessages[0] }), {
-        status: 400,
-      });
-    } else {
-      // Handle other errors here if needed
-      return new Response(JSON.stringify({ error: "An error occurred", errorMessage: error }), {
-        status: 500,
-      });
-    }
+    return new Response(JSON.stringify({ error: "An error occurred", errorMessage: error }), {
+      status: 500,
+    });
   }
 }
