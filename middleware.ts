a62a731dff3fd5d7137cb70fdc8b07521f59f150
KungLucas <lucas.larsson93@hotmail.com>
2023-10-11 23:00:17 +0200

    chore(): api for categories.

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // "/" will be accessible to all users
-  publicRoutes: ["/api/addProduct", "/api/getProduct", "/api/order", "/api/getProductById"],
+  publicRoutes: ["/api/addProduct", "/api/getProduct", "/api/order", "/api/getProductById", "/api/getCategory", "/api/addCategory", "/api/deleteCategory"],
 });
 
 export const config = {
