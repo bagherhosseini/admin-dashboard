057b2d4c9b56584377e79e82f8f8c643c5cd747b
Jonathan <jonteljungp@gmail.com>
2023-10-12 14:34:53 +0200

    Connected FE Rename with BE Rename API

--- app/(pages)/[storeId]/settings/components/Rename.tsx
+++ app/(pages)/[storeId]/settings/components/Rename.tsx
@@ -1,56 +1,85 @@
 "use client";
 
-import React from 'react'
-import { useState, useEffect} from 'react';
+import { useState } from 'react';
+import { useParams, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
 import { Input } from "@/components/ui/input";
+import { toast } from "react-hot-toast";
 
 export function Rename() {
+  const router = useRouter();
+  const params = useParams();
+  const storeId = params.storeId;
   const [storeName, setStoreName] = useState<string | null>(null);
+  const [loaded, setLoaded] = useState(false);
 
-  useEffect(() => {
-    // Get the storeId from the current URL
-    const pathSegments = window.location.pathname.split('/');
-  
-    const storeId = pathSegments[1];
+  if (!loaded) {
+    (async function fetchData() {
+      if (!storeId) {
+        console.error('Store ID not found in the URL.');
+        return;
+      }
 
-  if (!storeId) {
-    console.error('Store ID not found in the URL.');
-    return;
-  }
-  
-    // Define the async function within useEffect
-    const fetchStoreName = async () => {
       try {
         const response = await fetch(`http://localhost:3000/api/getStoreNameById?storeId=${storeId}`);
         const data = await response.json();
-        console.log(data);
-  
+
         if (data && data.name) {
           setStoreName(data.name);
+          setLoaded(true);  // Set loaded to true after fetching data
         } else {
           console.error('Error fetching store name:', data);
         }
       } catch (error) {
         console.error('Error fetching store name:', error);
       }
-    };
-  
-    // Call the async function
-    fetchStoreName();
-  
-  }, []); 
+    })();
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStoreName(e.target.value);
+  };
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (storeId && storeName) {
+      try {
+        const response = await axios.post('https://admin-dashboard-kappa-one.vercel.app/api/renameStore', 
+        { 
+          storeId,
+          name: storeName 
+          }
+          );
+        
+        // Handle the response as needed
+        if (response.status === 200) {
+          toast.success("Store successfully renamed!");
+          setStoreName(storeName);
+        }
+      } catch (error) {
+        console.error('Error updating store name:', error);
+        toast.error("Error updating store name")
+      }
+    }
+  };
+
+
   return (
     <div className="space-y-8 w-full">
-      <form className="space-y-8 w-full">
+      <form onSubmit={handleFormSubmit} className="space-y-8 w-full">
         <div className="grid grid-cols-3 gap-8">
           <div className="space-y-2">
             <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Name</label>
-            <Input className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" placeholder={storeName || "Loading..."} />
+            <Input 
+              value={storeName || ''}
+              onChange={handleInputChange}
+              className="..."
+              placeholder={storeName || "Loading..."} 
+            />
           </div>
         </div>
         <Button type="submit">Save Changes</Button>
-        <div data-orientation="horizontal" role="none" className="shrink-0 bg-border h-[1px] w-full"></div>
       </form>
     </div>
   );
