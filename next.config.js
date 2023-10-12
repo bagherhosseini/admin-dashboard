6ef0b5f9d160fbf038c7ea0ea260f02a9ce1ce0d
Bagher hosseini <bagherhosseini03@gmail.com>
2023-10-12 21:27:00 +0200

    fix(CORS): add Access-Control-Allow-Origin

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
     images: {
         domains: ["res.cloudinary.com"]
+    },
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "*" }
+                ]
+            }
+        ]
     }
 }
 
