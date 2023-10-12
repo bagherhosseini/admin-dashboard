d2a78cfe57fdc48b2294b84c4d0d7fcba465f70f
Bagher hosseini <bagherhosseini03@gmail.com>
2023-10-12 09:10:37 +0200

    fix: fix middleware and package-lock

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
-export default authMiddleware({
-  // "/" will be accessible to all users
+
 export default authMiddleware({
   publicRoutes: ["/api/:path*"],
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
