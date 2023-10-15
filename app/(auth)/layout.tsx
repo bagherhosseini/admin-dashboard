38599840846710a23a18b2a9931a7bb67b551144
Bagher hosseini <bagherhosseini03@gmail.com>
2023-10-15 13:12:17 +0200

    fix: heigh of SignInLayout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function SignInLayout({
     children: React.ReactNode
   }) {
     return (
-      <div className="flex items-center justify-center h-full w-full">
+      <div className="flex items-center justify-center h-screen w-full">
         {children}
       </div>
     );
